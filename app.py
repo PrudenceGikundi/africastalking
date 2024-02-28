ed6f2d946e1a7846441948aa216c074e492041f4
PrudenceGikundi <prudencekendi101@gmail.com>
2024-02-28 15:20:44 +0300

    com2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import africastalking
+import os
 
 
 
@@ -56,4 +57,4 @@ def ussd_callback():
     return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="3000")
+    app.run(host="0.0.0.0", port=os.environ.get("PORT"))
